ec4f841494ab271552434fbc68118473f4357d68
Davaughn1 <107500041+Davaughn1@users.noreply.github.com>
2022-06-14 13:14:26 -0400

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,21 @@
+
+$("button").click(function() {
+    
+ let input;
+    input=$(".input").val();
+    
+	$("vote-result").text(input);
+	    
+    alert(input);
+   
+    
+});
+
+$("button").click(function(){
+
+let inout;
+    inout=$(".inout").val();
+    $("amount of yotes").text(inout);
+alert(inout);
+
+});
